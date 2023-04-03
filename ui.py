dbd0bfad82fb37fb1dc7f409da98eeebcb072470
zhangxy <758109892@qq.com>
2023-04-03 15:12:53 +0800

    1.优化了线程卡顿 2.增加了手动强停和自动记录进程重启 3.去除了qt本身的线程代码

--- ui.py
+++ ui.py
@@ -1,5 +1,4 @@
 import threading
-
 from PySide2.QtWidgets import QApplication, QDialog, QVBoxLayout, QPushButton
 import pyqtgraph as pg
 from PySide2.QtUiTools import QUiLoader
@@ -9,27 +8,27 @@ import os, re, requests, json, urllib
 from PySide2.QtCore import QCoreApplication, QObject, QRunnable, QThreadPool, QTimer, Qt, QCoreApplication
 from pyqtgraph.Qt import QtCore
 from PySide2.QtWidgets import QInputDialog, QLineEdit
+import datetime
 
 
-class Worker(QRunnable):
-    def __init__(self, parent=None):
-        super(Worker, self).__init__(parent)
-        self.should_stop = False
-
-    def run(self):
-        while not self.should_stop:
-            # 在这里执行操作，如果要停止线程，请设置 self.should_stop 为 True，然后跳出循环
-            pass
+# class Worker(QRunnable):
+#     def __init__(self, parent=None):
+#         super(Worker, self).__init__(parent)
+#         self.should_stop = False
+#
+#     def run(self):
+#         while not self.should_stop:
+#             # 在这里执行操作，如果要停止线程，请设置 self.should_stop 为 True，然后跳出循环
+#             pass
 
 
 class Stats:
 
     def __init__(self):
+        self.old_pid = None
         self.packname = None
-        self.threadpool = None
-        self.processes = None
-        self.worker = None
-
+        self.force_stop_num = 0
+        self.reset_process_num = 0
         self.i = None
         self.x = None
         self.y = None
@@ -42,6 +41,7 @@ class Stats:
         self.ui.process_start.clicked.connect(self.start_plot)
         self.ui.process_clear.clicked.connect(self.clear_process)
         self.ui.stop_button.clicked.connect(self.stop_timer)
+        self.ui.force_stop.clicked.connect(self.force_stop)
         # 提示框
         pack_name_button = QPushButton('OK')
         self.pack_name_line = QLineEdit('')
@@ -50,6 +50,10 @@ class Stats:
         VBoxLayout.addWidget(pack_name_button)
         self.Dialog = QDialog()
         self.Dialog.setLayout(VBoxLayout)
+
+    def print_text(self, function):
+        content = f'{datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}\n==============start=================\n{function}\n'
+        self.ui.result_label.insertPlainText(content)
 
     # 获取当前进程pid
     def pid_now(self):
@@ -94,6 +98,9 @@ class Stats:
     # 进程栏
     def clear_process(self):
         self.ui.historyPlot.clear()
+        self.ui.X_line.clear()
+        self.ui.Y_line.clear()
+        self.ui.time_line.clear()
 
     # 获取输入的包名
     def get_packname(self):
@@ -104,24 +111,20 @@ class Stats:
     def stop_timer(self):
         self.update_timer.stop()  # 停止 QTimer
 
-    # 关闭进程
-    def closeEvent(self, event):
-        # 遍历所有进程并终止它们
-        for process in self.processes:
-            process.terminate()
-
-        event.accept()
+    def force_stop(self):
+        self.force_stop_num += 1
+        cmd = f'adb shell am force-stop {self.get_packname()}'
+        os.popen(cmd)
+        self.print_text(f'手动强停{self.force_stop_num}次')
 
     # 实时更新图
-
     def start_plot(self):
         if str(self.get_packname()) is None:
             self.Dialog.show()
 
         self.i = 0
-        self.x = []
-        self.y = []
-
+        self.x = [0]
+        self.y = [0]
         self.update_timer = QtCore.QTimer()
         self.update_timer.timeout.connect(self.updateData)
         self.update_timer.start(1000)  # 1000ms更新一次
@@ -129,6 +132,7 @@ class Stats:
         thread.start()
 
     def updateData(self):
+        self.old_pid = self.y[-1]
         self.i += 1
         self.x.append(self.i)
         self.y.append(float(self.pid_now()))
@@ -136,11 +140,29 @@ class Stats:
         # 更新绘图
         self.ui.historyPlot.clear()
         self.ui.historyPlot.plot(self.x, self.y)
+        self.ui.X_line.setText('')
+        self.ui.X_line.setText(str(self.x[-1]))
+        self.ui.Y_line.setText('')
+        self.ui.Y_line.setText(str(self.y[-1]))
+        self.ui.time_line.setText('')
+        self.ui.time_line.setText(str(datetime.datetime.now().strftime("%H:%M:%S")))
+        self.last_pid = self.y[-1]
+        thread = threading.Thread(target=self.reset_process, args=(self.last_pid, self.old_pid))
+        thread.start()
+
+    # 进程重置节点记录
+    def reset_process(self, last_pid, old_pid):
+        if int(old_pid) == 0:
+            pass
+        elif str(last_pid) != str(old_pid):
+            self.reset_process_num += 1
+            self.print_text(f'进程重启{self.reset_process_num}次\n')
+        else:
+            pass
 
 
 if __name__ == '__main__':
     app = QApplication([])
     stats = Stats()
     stats.ui.show()
-    # app.exec_()
     sys.exit(app.exec_())
